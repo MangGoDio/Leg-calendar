7204c018496abb0c0829c42657d86540780c65a7
超级战士 <492179450@qq.com>
2018-06-29 14:58:36 +0800

    兼容IOS

--- src/js/init_date.js
+++ src/js/init_date.js
@@ -1,110 +1,119 @@
-import { getWeek, getDayInfo, diffDays } from './utils'
-import { __BENCH, __PLACE } from './config'
-import { showCalendar } from './show_calendar'
+import { getWeek, getDayInfo, diffDays } from "./utils";
+import { __BENCH, __PLACE } from "./config";
+import { showCalendar } from "./show_calendar";
 // 保存上一天超出的日期
-let prevDay = {}
+let prevDay = {};
 
 // 初始化当前月份表格
 export const initTable = () => {
+  //  重置上个月的余留时间
+  prevDay = {};
 
-    //  重置上个月的余留时间
-    prevDay = {}
+  // 获取当前年份和月份
+  const dayArr = [],
+    { year, month } = window.DateInfo.current;
 
-    // 获取当前年份和月份
-    const dayArr = [], { year, month } = window.DateInfo.current
+  // 定义到当前月份1号的星期数
+  const curWeek = getWeek(year, month, 1);
 
-    // 定义到当前月份1号的星期数
-    const curWeek = getWeek(year, month, 1)
-
-    // 遍历，确保第一个数组是星期日
-    // 将上月数组添加到arr
-    if (curWeek !== 0) {
-        for (let i = 0; ; i--) {
-            let date = getDayInfo(year, month, i, 'other')
-            dayArr.unshift(date)
-            if (date.week === 0) break
-        }
+  // 遍历，确保第一个数组是星期日
+  // 将上月数组添加到arr
+  if (curWeek !== 0) {
+    for (let i = 0; ; i--) {
+      let date = getDayInfo(year, month, i, "other");
+      dayArr.unshift(date);
+      if (date.week === 0) break;
     }
+  }
 
-    // 遍历本月数组，添加到arr
-    for (let i = 1; ; i++) {
-        let date = getDayInfo(year, month, i)
-        if (date.month !== month) break
-        dayArr.push(date)
+  // 遍历本月数组，添加到arr
+  for (let i = 1; ; i++) {
+    let date = getDayInfo(year, month, i);
+    if (date.month !== month) break;
+    dayArr.push(date);
+  }
+
+  // 判定本月结尾星期数
+  // 遍历，直到最后一个数组是星期六
+  // 将下月数组添加到arr
+  const lastWeek = dayArr[dayArr.length - 1].week,
+    lastDay = dayArr[dayArr.length - 1].day;
+  if (lastWeek !== 6) {
+    for (let i = lastDay + 1; ; i++) {
+      let date = getDayInfo(year, month, i, "other");
+      dayArr.push(date);
+      if (date.week === 6) break;
     }
+  }
 
-    // 判定本月结尾星期数
-    // 遍历，直到最后一个数组是星期六
-    // 将下月数组添加到arr
-    const lastWeek = dayArr[dayArr.length - 1].week,
-        lastDay = dayArr[dayArr.length - 1].day
-    if (lastWeek !== 6) {
-        for (let i = lastDay + 1; ; i++) {
-            let date = getDayInfo(year, month, i, 'other')
-            dayArr.push(date)
-            if (date.week === 6) break
-        }
-    }
+  window.DateInfo.arr = dayArr;
 
-    window.DateInfo.arr = dayArr
-
-    addLegInfo()
-}
+  addLegInfo();
+};
 
 // 插入独有的军团信息
 const addLegInfo = () => {
+  // 7月份以前的不插入
+  const { year, month } = window.DateInfo.current;
 
-    // 7月份以前的不插入
-    const { year, month } = window.DateInfo.current
-
-    if (year > 2017 || year === 2017 && month > 6) {
-        for (let i = 0, dio = window.DateInfo.arr.length; i < dio; i++) {
-            window.DateInfo.arr[i] = legDay(window.DateInfo.arr[i])
-        }
+  if (year > 2017 || (year === 2017 && month > 6)) {
+    for (let i = 0, dio = window.DateInfo.arr.length; i < dio; i++) {
+      window.DateInfo.arr[i] = legDay(window.DateInfo.arr[i]);
     }
+  }
 
-    showCalendar()
-
-}
-
+  showCalendar();
+};
 
 // 计算某一天的军团信息
 const legDay = info => {
-    // 初始化当前数组第一天
-    const diffObj = diffDays(__BENCH.date, `${window.DateInfo.current.year}-${info.month + 1}-${info.day}`)
-    // 循环填装数组
-    let time = __BENCH.time
-    for (let i = 0; i < diffObj.days; i++) {
-        time += 18.5
-        if (time > 24) {
-            time -= 24
-        } else {
-            diffObj.days++
-        }
-    }
-
-    let arr = [], end = ''
-    // 如果有上一天遗留下来的数据，加入
-    if (prevDay.place) {
-        arr.push(prevDay)
-        prevDay = {}
-    }
-    // 计算第一天是否有超出时间
-    if (time + 6 > 24) {
-        end = 24
-        prevDay = { place: __PLACE[(diffObj.days) % 12], time: 0, end: time + 6 - 24 }
+  // 初始化当前数组第一天
+  const diffObj = diffDays(
+    __BENCH.date,
+    `${window.DateInfo.current.year}/${info.month + 1}/${info.day}`
+  );
+  // 循环填装数组
+  let time = __BENCH.time;
+  for (let i = 0; i < diffObj.days; i++) {
+    time += 18.5;
+    if (time > 24) {
+      time -= 24;
     } else {
-        end = time + 6
+      diffObj.days++;
     }
-    arr.push({ place: __PLACE[(diffObj.days) % 12], time, end })
-    time += 18.5
-    if (time < 24) {
-        // 计算第一天第二段超出时间
-        prevDay = { place: __PLACE[(diffObj.days) % 12 + 1], time: 0, end: time + 6 - 24 }
-        arr.push({ place: __PLACE[(diffObj.days) % 12 + 1], time, end: 24 })
-    }
+  }
 
-    info.leg = arr
+  let arr = [],
+    end = "";
+  // 如果有上一天遗留下来的数据，加入
+  if (prevDay.place) {
+    arr.push(prevDay);
+    prevDay = {};
+  }
+  // 计算第一天是否有超出时间
+  if (time + 6 > 24) {
+    end = 24;
+    prevDay = {
+      place: __PLACE[diffObj.days % 12],
+      time: 0,
+      end: time + 6 - 24
+    };
+  } else {
+    end = time + 6;
+  }
+  arr.push({ place: __PLACE[diffObj.days % 12], time, end });
+  time += 18.5;
+  if (time < 24) {
+    // 计算第一天第二段超出时间
+    prevDay = {
+      place: __PLACE[(diffObj.days % 12) + 1],
+      time: 0,
+      end: time + 6 - 24
+    };
+    arr.push({ place: __PLACE[(diffObj.days % 12) + 1], time, end: 24 });
+  }
 
-    return info
-}
+  info.leg = arr;
+
+  return info;
+};
--- src/js/utils.js
+++ src/js/utils.js
@@ -1,44 +1,45 @@
-const log = console.log.bind(console)
+const log = console.log.bind(console);
 
-export const getDom = str => document.querySelector(str)
+export const getDom = str => document.querySelector(str);
 
 // 获取当前星期数
-export const getWeek = (year, month, num) => new Date(year, month, num).getDay()
+export const getWeek = (year, month, num) =>
+  new Date(year, month, num).getDay();
 
 // 获取当前日期信息
 export const getDayInfo = (year, month, num, status) => {
-    const date = new Date(year, month, num)
-    return {
-        day: date.getDate(),
-        week: date.getDay(),
-        month: date.getMonth(),
-        status: status || 'current'
-    }
-}
+  const date = new Date(year, month, num);
+  return {
+    day: date.getDate(),
+    week: date.getDay(),
+    month: date.getMonth(),
+    status: status || "current"
+  };
+};
 
 // 计算两个日期相差天数
 export const diffDays = (day1, day2) => {
-    let obj = {
-        days: 0,
-        new: 0,
-    }
+  let obj = {
+    days: 0,
+    new: 0
+  };
+  console.log(day2);
+  const diff = Date.parse(day2) - Date.parse(day1);
+  if (diff === 0) {
+    return obj;
+  } else {
+    obj.new = diff > 0 ? 1 : -1;
+  }
 
-    const diff = Date.parse(day2) - Date.parse(day1)
-    if (diff === 0) {
-        return obj
-    } else {
-        obj.new = diff > 0 ? 1 : -1
-    }
+  obj.days = Math.floor(Math.abs(diff) / (24 * 3600 * 1000));
 
-    obj.days = Math.floor(Math.abs(diff) / (24 * 3600 * 1000))
-
-    return obj
-}
+  return obj;
+};
 
 // 解析时间
 export const numToTime = time => {
-    const str = time.toString(),
-        hours = str.split('.')[0],
-        mins = str.split('.')[1]
-    return mins ? `${hours}:30` : `${hours}:00`
-}
+  const str = time.toString(),
+    hours = str.split(".")[0],
+    mins = str.split(".")[1];
+  return mins ? `${hours}:30` : `${hours}:00`;
+};
